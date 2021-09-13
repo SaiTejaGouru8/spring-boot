da145a6affe37a3ada2893ff91f09a83f3b2a9e3
SaiTeja Gouru <saigouru8@gmail.com>
2021-09-13 18:27:33 +0100

    updated

--- pipeline.groovy
+++ pipeline.groovy
@@ -38,8 +38,8 @@ node(){
     
     stage("deploy"){
         bat """
-         curl "http://localhost:8888/repository/maven-public/com/spring-boot/spring-boot/${env.BUILD_NUMBER}/spring-boot-${env.BUILD_NUMBER}.jar" --output spring-boot-1.0.jar
-         Xcopy "spring-boot-${env.BUILD_NUMBER}.jar" "C:\\xampp\\Tomcat\\webapps\\"
+         curl "http://localhost:8888/repository/maven-public/com/spring-boot/spring-boot/${env.BUILD_NUMBER}/spring-boot-${env.BUILD_NUMBER}.jar" --output spring-boot.jar
+         Xcopy "spring-boot.jar" "C:\\xampp\\Tomcat\\webapps\\"
 
         """
     }
