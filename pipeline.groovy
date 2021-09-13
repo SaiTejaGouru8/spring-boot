bf509b9e6da3812526add18a549b3fe140560f57
SaiTeja Gouru <saigouru8@gmail.com>
2021-09-13 17:53:12 +0100

    updated

---
+++ pipeline.groovy
@@ -0,0 +1,43 @@
+node(){
+    deleteDir()
+    
+    stage("git checkout"){
+        git url: "https://github.com/SaiTejaGouru8/spring-boot.git"
+    }
+    
+    stage("gradle build"){
+        bat"""
+       
+        gradlew build
+        """
+        
+
+    }
+    
+    stage("junit"){
+        bat"""
+        gradlew test
+        """
+        junit '**/*.xml'
+
+    }
+    
+    stage("sonar analysis"){
+        bat"""
+        gradlew sonarqube -Dsonar.host.url=http://localhost:9000 -Dsonar.login=3ad9534c6ea3a9111fb6ef00d8a1a60b6105b0cb
+        """
+        
+
+    }
+    
+    stage("nexus upload"){
+        bat """
+        gradlew publish
+        """
+    }
+    
+    stage("deploy"){
+        
+    }
+    
+}
